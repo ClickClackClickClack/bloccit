74d5379726d4a9f53bf58dcc25c0644e762d4cbb
Jon Reck <jonathonreck@gmail.com>
2018-01-21 09:55:07 -0500

    authentication checkpoint complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   resources :topics do
     resources :posts, except: [:index]
-  end  
+  end
 
+  resources :users, only: [:new, :create]
+  
   get 'about' => 'welcome#about'
 
   root 'welcome#index'
