faf9fb0a13d29278c752b477fb4f912933150d2e
Jon Reck <jonathonreck@gmail.com>
2018-01-28 10:38:55 -0500

    roles checkpoint complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,9 +35,16 @@ topics = Topic.all
    )
  end
 
- user = User.first
- user.update_attributes!(
-   email: 'jonathonreck@gmail.com',
+ admin = User.create!(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+ 
+ member = User.create!(
+   name:     'Member User',
+   email:    'member@example.com',
    password: 'helloworld'
  )
 
