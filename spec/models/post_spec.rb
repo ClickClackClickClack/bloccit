710cf4dd0388049ad6a9298537500bb12f83079a
Jon Reck <jonathonreck@gmail.com>
2018-02-02 08:29:38 -0500

    auto upvote added for creator of post

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -72,5 +72,11 @@ RSpec.describe Post, type: :model do
          expect(post.rank).to eq (old_rank - 1)
        end
      end
+
+     describe "#create_vote" do
+       it "associates the first vote with the user that created the post" do
+         expect(post.votes.first.user).to eq(post.user)
+       end
+     end
    end
 end
